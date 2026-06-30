8ccd2141d5d6cd3cda4403aeaed6cce8b0b15c62
asyaustadimiz <ustadimizasya@gmail.com>
2026-06-30 19:52:45 +0300

    melman init

---
+++ melman.js
@@ -0,0 +1,200 @@
+[
+   {
+       "date": "2026-06-09",
+       "weight": 64.9,
+       "bloodPressure": {
+           "systolic": 0,
+           "diastolic": 0,
+           "pulse": 0
+       }
+   },
+   {
+       "date": "2026-06-10",
+       "weight": 63.2,
+       "bloodPressure": {
+           "systolic": 0,
+           "diastolic": 0,
+           "pulse": 0
+       }
+   },
+   {
+       "date": "2026-06-11",
+       "weight": 63.5,
+       "bloodPressure": {
+           "systolic": 0,
+           "diastolic": 0,
+           "pulse": 0
+       }
+   },
+   {
+       "date": "2026-06-12",
+       "weight": 62.4,
+       "bloodPressure": {
+           "systolic": 12.5,
+           "diastolic": 7.8,
+           "pulse": 73
+       }
+   },
+   {
+       "date": "2026-06-13",
+       "weight": 62.3,
+       "bloodPressure": {
+           "systolic": 11.8,
+           "diastolic": 7.8,
+           "pulse": 72
+       }
+   },
+   {
+       "date": "2026-06-14",
+       "weight": 63.4,
+       "bloodPressure": {
+           "systolic": 10.9,
+           "diastolic": 6.5,
+           "pulse": 70
+       }
+   },
+   {
+       "date": "2026-06-15",
+       "weight": 63.8,
+       "bloodPressure": {
+           "systolic": 11.5,
+           "diastolic": 8.4,
+           "pulse": 76
+       }
+   },
+   {
+       "date": "2026-06-16",
+       "weight": 63.3,
+       "bloodPressure": {
+           "systolic": 11.5,
+           "diastolic": 6.5,
+           "pulse": 84
+       }
+   },
+   {
+       "date": "2026-06-17",
+       "weight": 63.0,
+       "bloodPressure": {
+           "systolic": 10.5,
+           "diastolic": 7.5,
+           "pulse": 75
+       }
+   },
+   {
+       "date": "2026-06-18",
+       "weight": 62.7,
+       "bloodPressure": {
+           "systolic": 11.0,
+           "diastolic": 7.7,
+           "pulse": 79
+       }
+   },
+   {
+       "date": "2026-06-19",
+       "weight": 62.1,
+       "bloodPressure": {
+           "systolic": 11.8,
+           "diastolic": 8.2,
+           "pulse": 98
+       }
+   },
+   {
+       "date": "2026-06-20",
+       "weight": 61.6,
+       "bloodPressure": {
+           "systolic": 11.8,
+           "diastolic": 7.9,
+           "pulse": 83
+       }
+   },
+   {
+       "date": "2026-06-21",
+       "weight": 62.1,
+       "bloodPressure": {
+           "systolic": 10.7,
+           "diastolic": 8.1,
+           "pulse": 87
+       }
+   },
+   {
+       "date": "2026-06-22",
+       "weight": 62.1,
+       "bloodPressure": {
+           "systolic": 9.8,
+           "diastolic": 7.5,
+           "pulse": 80
+       }
+   },
+   {
+       "date": "2026-06-23",
+       "weight": 62.2,
+       "bloodPressure": {
+           "systolic": 0,
+           "diastolic": 0,
+           "pulse": 0
+       }
+   },
+   {
+       "date": "2026-06-24",
+       "weight": 61.6,
+       "bloodPressure": {
+           "systolic": 9.5,
+           "diastolic": 7.6,
+           "pulse": 84
+       }
+   },
+   {
+       "date": "2026-06-25",
+       "weight": 61.6,
+       "bloodPressure": {
+           "systolic": 11.3,
+           "diastolic": 8.2,
+           "pulse": 102
+       }
+   },
+   {
+       "date": "2026-06-26",
+       "weight": 62,
+       "bloodPressure": {
+           "systolic": 10.9,
+           "diastolic": 7.4,
+           "pulse": 92
+       }
+   },
+   {
+       "date": "2026-06-27",
+       "weight": 61.3,
+       "bloodPressure": {
+           "systolic": 11.7,
+           "diastolic": 7.6,
+           "pulse": 105
+       }
+   },
+   {
+       "date": "2026-06-28",
+       "weight": 61.5,
+       "bloodPressure": {
+           "systolic": 10.6,
+           "diastolic": 7.9,
+           "pulse": 80
+       }
+   },
+   {
+       "date": "2026-06-29",
+       "weight": 62.3,
+       "bloodPressure": {
+           "systolic": 9.7,
+           "diastolic": 7.7,
+           "pulse": 81
+       }
+   },
+   {
+       "date": "2026-06-30",
+       "weight": 62.1,
+       "bloodPressure": {
+           "systolic": 12.5,
+           "diastolic": 9.0,
+           "pulse": 98
+       }
+   }
+]
